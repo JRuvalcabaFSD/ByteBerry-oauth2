22c1ca703a0eb3a33e9d8e19d56ea41f0b9afe6e
Jesús Ruvalcaba <jesus@jrmdev.org>
2025-12-07 08:09:03 -0600

    fix:  change version variable in update script

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -5,7 +5,7 @@ if [ -n "$BUILD_VERSION" ]; then
     node -e "
         const fs = require('fs');
         const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-        pkg.version = process.env.BUILD_VERSION;
+        pkg.version = process.env.APP_VERSION;
         fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2) + '\n');
     " || exit 1
 fi
